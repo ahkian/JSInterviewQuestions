33a4ffd1cc61b3ec2bf47789c9185e9a96275f1b
Daniel Brazel <dbrazel4@gmail.com>
2018-11-21 11:22:36 -0500

    called vignere's function

--- algorithmPractice.js
+++ algorithmPractice.js
@@ -273,6 +273,8 @@ function vigeneresCipher(string, keySeq){
   return encodedStr
 }
 
+vigeneresCipher("zebra", [1,2,3])
+
 /* An isogram is a word that has no repeating letters, consecutive or non-consecutive.
 Implement a function that determines whether a string that contains only letters is an isogram.
 Assume the empty string is an isogram. Ignore letter case. */
